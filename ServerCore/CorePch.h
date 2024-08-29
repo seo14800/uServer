f9400c0dea694f3e655328a570907686110c9d95
seo14800 <seo14800@gmail.com>
2024-08-29 22:25:06 +0900

    초기 커밋.

---
+++ ServerCore/CorePch.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Types.h"
+#include "CoreMacro.h"
+#include "CoreTLS.h"
+#include "CoreGlobal.h"
+
+#include <array>
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+#define _WINSOCKAPI_	// WinSock2.h와 Windows.h 충돌 해결
+#include <Windows.h>
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+#include <winsock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+#include "SendBuffer.h"
+#include "Session.h"
+#include "JobQueue.h"
+
+
---
+++ uServer/uServer.cpp
@@ -0,0 +1,63 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "ThreadManager.h"
+#include "Service.h"
+#include "Session.h"
+#include "GameSession.h"
+#include "GameSessionManager.h"
+#include <tchar.h>
+#include "Job.h"
+#include "Protocol.pb.h"
+#include "Room.h"
+
+enum
+{
+	WORKER_TICK = 64
+};
+
+void DoWorkerJob(ServerServiceRef& service)
+{
+	while (true)
+	{
+		LEndTickCount = ::GetTickCount64() + WORKER_TICK;
+
+		service->GetIocpCore()->Dispatch(10);
+
+		ThreadManager::DistributeReservedJobs();
+
+		ThreadManager::DoGlobalQueueWork();
+	}
+}
+
+int main()
+{
+	ServerPacketHandler::Init();
+
+	ServerServiceRef service = make_shared<ServerService>(
+		NetAddress(L"127.0.0.1", 7777),
+		make_shared<IocpCore>(),
+		[=]() { return make_shared<GameSession>(); }, // TODO : SessionManager 등
+		100);
+
+	ASSERT_CRASH(service->Start());
+
+	for (int32 i = 0; i < 16; i++)
+	{
+		GThreadManager->Launch([&service]()
+			{
+				DoWorkerJob(service);
+			});
+	}
+
+	// Main Thread
+	//DoWorkerJob(service);
+
+	GRoom->DoAsync(&Room::UpdateTick);
+
+	while (true)
+	{
+		this_thread::sleep_for(0.1s);
+	}
+
+	GThreadManager->Join();
+}
